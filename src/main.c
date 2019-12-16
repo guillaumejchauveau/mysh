055798e87b7c4126f041e52bb423b36d7f926b0f
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2019-12-16 15:22:17 +0100

    Fix

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
+
 #include <errno.h>
 #include <error.h>
 #include <readline/history.h>
 #include <readline/readline.h>
 #include <signal.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
