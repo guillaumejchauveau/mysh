e775bd892ec6681bd8ffc19e7c4ee13f6323451e
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2020-01-02 15:02:53 +0100

    Fix undefined behavior

--- src/main.c
+++ src/main.c
@@ -77,6 +77,8 @@ int main(int argc, char **argv) {
 
   struct sigaction sa;
   sa.sa_handler = sigint_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
   sigaction(SIGINT, &sa, NULL);
 
   int status = 0, line_id = 1;
